87a520b5ff389b7f88d0d571f2d2c3a01216011d
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2025-07-22 11:20:15 -0400

    set up project structure

---
+++ src/components/card.js
@@ -0,0 +1,16 @@
+export default function Card({
+  card,
+  onHover,
+  onClick,
+}) {
+  return (
+    <div>
+
+
+
+
+
+
+    </div>
+  )
+}
---
+++ src/lib/data.js
@@ -0,0 +1,28 @@
+export const initialCards = [{
+  id: 0,
+  name: "Krark's Thumb",
+  ability: 'Flip 2 coins, ignore 1.',
+  firesOn: null,
+  isActive: false,
+}, {
+  id: 1,
+  name: 'Zndrsplt, Eye of Wisdom',
+  ability: 'Draw a card.',
+  firesOn: 'heads',
+  isActive: false,
+}, {
+  id: 2,
+  name: 'Okaun, Eye of Chaos',
+  ability: "Double Okrain's power/toughness.",
+  firesOn: 'heads',
+  isActive: false,
+  power: 3,
+  toughness: 3,
+}, {
+  id: 3,
+  name: 'Mana Crypt',
+  ability: 'Deals 3 damage to you.',
+  firesOn: 'tails',
+  isActive: false,
+}
+]
